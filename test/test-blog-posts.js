12ca257e8cb23bc8c48e067025c548f7cd8358bb
sollertis8 <mizumedia@gmail.com>
2017-11-27 16:32:04 -0500

    Added seedBlogPostData() function
    Added seedBlogPostData() function to test/test-blog-posts.js

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -12,3 +12,11 @@ const {TEST_DATABAASE_URL} = require('../config');
 
 chai.use(chaiHttp);
 
+function seedBlogPostData() {
+    const seedData = {};
+
+    for (let i=1; i<=10; i++) {
+        seedData.push(generateBlogPostData());
+    }
+    return BlogPost.insertMany(seedData);
+}
