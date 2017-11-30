03aa675e87c922abb2167a7ae27e7a7ddafdc4cb
sollertis8 <mizumedia@gmail.com>
2017-11-30 15:28:42 -0500

    Added teardown function

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -1,22 +1,49 @@
 const chai = require('chai');
-const chaiHttp = require ('chai-http');
-const faker = require ('faker');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
 const mongoose = require('mongoose');
 
 
 const should = chai.should();
 
-const {BlogPost} = require('../models');
-const {app, runServer, closeServer} = require ('../server');
-const {TEST_DATABAASE_URL} = require('../config');
+const {
+    DATABASE_URL
+} = require('../config');
+const {
+    BlogPost
+} = require('../models');
+const {
+    app,
+    runServer,
+    closeServer
+} = require('../server');
+const {
+    TEST_DATABAASE_URL
+} = require('../config');
 
 chai.use(chaiHttp);
 
-function seedBlogPostData() {
-    const seedData = {};
+function tearDownDb() {
+    return new Promise((resolve, reject) => {
+        console.warn('Deleting database');
+        mongoose.connection.dropDatabase()
+            .then(result => resolve(result))
+            .catch(err => reject(err))
+    });
+}
 
-    for (let i=1; i<=10; i++) {
-        seedData.push(generateBlogPostData());
+function seedBlogPostData() {
+    const seedData = [];
+
+    for (let i = 1; i <= 10; i++) {
+        seedData.push({
+            author: {
+                firstName: faker.name.firstName(),
+                lastName: faker.name.lastName()
+            },
+            title: faker.lorem.sentence(),
+            content: faker.lorem.text()
+        });
     }
     return BlogPost.insertMany(seedData);
 }
