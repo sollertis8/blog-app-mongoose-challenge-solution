39291bd02dbae18d6a0599f69162ecacd7d7d513
sollertis8 <mizumedia@gmail.com>
2017-11-30 17:29:57 -0500

    Added Test URL

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb://localhost/sollertis-blog-db';
+    global.DATABASE_URL ||
+    'mongodb://localhost/sollertis-blog-db';
+exports.TEST_DATABASE_URL = (
+    process.env.TEST_DATABASE_URL ||
+    'mongodb://localhost/test/test-blog-posts');
 exports.PORT = process.env.PORT || 8080;
