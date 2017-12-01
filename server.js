0e3f054c60a5ef152ad80234e81da7b1dc47760c
sollertis8 <mizumedia@gmail.com>
2017-12-01 14:44:27 -0500

    refactored server.js

--- server.js
+++ server.js
@@ -157,4 +157,4 @@ if (require.main === module) {
   runServer().catch(err => console.error(err));
 };
 
-module.exports = {runServer, app, closeServer};
+module.exports = {runServer, app, closeServer}; 
